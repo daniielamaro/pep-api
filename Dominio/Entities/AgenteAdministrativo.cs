55055138d6907169276a01d800350e371f76838a
Daniel Amaro <daniel.amaro.rj@gmail.com>
2021-11-16 18:30:26 -0300

    domain novos clinicas

---
+++ Dominio/Entities/AgenteAdministrativo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entities
+{
+    public class AgenteAdministrativo : Entidade
+    {
+        public string Nome { get; set; }
+        public Arquivo FotoPerfil { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Clinica Clinica { get; set; }
+    }
+}
---
+++ Dominio/Entities/Enfermeiro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entities
+{
+    public class Enfermeiro : Entidade
+    {
+        public string Nome { get; set; }
+        public Arquivo FotoPerfil { get; set; }
+        public string COREM { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Clinica Clinica { get; set; }
+        public List<Medicamento> Medicamentos { get; set; }
+        public List<Consulta> Consultas { get; set; }
+    }
+}
